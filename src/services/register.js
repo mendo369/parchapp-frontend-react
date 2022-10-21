f0279a2dee1edba3263bece6118af917f52bfc18
Luis Mendoza <luis23luis33@gmail.com>
2022-10-22 00:36:16 +0200

    Deploy

--- src/services/register.js
+++ src/services/register.js
@@ -5,7 +5,6 @@ export default function register({ userName, name, email, avatar, password }) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({ userName, name, email, avatar, password }),
   }).then((res) => {
